a24d9579847d46822a545e83123f906e4a399de8
WojciuZ <ww.potoczek@gmail.com>
2025-03-21 16:09:32 +0000

    Week 7

---
+++ Week 7/Practice Java/script.js
@@ -0,0 +1,50 @@
+// 1.Variables
+// let cat = "Rex" //initialation of variables
+// console.log(cat);
+
+
+
+// 2.Datatypes in programming
+// Int
+// String
+// Float
+// Boolean
+
+// 3.Loops and decisions
+// let age = 20;
+
+// if (age <14){
+//     alert("Child ticket")
+// }
+// else if (age<65) {
+//     alert("Adult ticket")
+// }
+// else {
+//     alert("Travel free")
+// }
+
+
+// let rain = true
+
+// if (rain) {
+//     alert ("Take your umbrella.")
+// }
+// else {
+//     alert ("No need for umbrella.")
+// }
+
+// 4. Functions
+// function checkWehther(rain){
+//     if (rain) {
+//     alert ("Take your umbrella.")
+//     }
+//     else {
+//     alert ("No need for umbrella.")
+//     }
+
+// }
+
+// 5.Controlling th webpage: the DOM
+
+// Document Object Model
+document.getElementById("example").style.background = "brown";
